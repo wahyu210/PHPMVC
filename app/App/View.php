05eca9c252f49b8689306af4467b22fafed96289
Mochamad Wahyu <wahyu212122@gmail.com>
2024-09-04 09:33:06 +0700

    updatee

---
+++ app/App/View.php
@@ -0,0 +1,7 @@
+<?php
+namespace MochamadWahyu\Phpmvc\App;
+class View{
+    public static function render (string $view,$model){
+        require __DIR__ . '/../view/'.$view.'.php bcxA  QW3';
+    }
+}
--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,12 +1,14 @@
 <?php
 namespace MochamadWahyu\Phpmvc\Controller;
+use MochamadWahyu\Phpmvc\App\View;
 class HomeController{
     function index(){
         $model = [
             'title'=>'Belajar PHP MVC',
             'content'=>'selamat belajar PHP MVC di channel programmer zaman now'
         ];
-        echo "HomeController.index()";
+        // echo "HomeController.index()";
+        View::render('Home/index', $model);
     }
     function hello():void{
         echo "HomeController.hello()";
@@ -16,5 +18,16 @@ class HomeController{
     }
     function about(){
         echo "AUTHOR : Mochamad Wahyu";
+    }
+    function Login():void{
+        $request = [
+            "username"=>$_POST['username'],
+            "password" => $_POST['username']
+        ];
+        $user = [];
+
+        $response=[
+            'message'=>'login sukses'
+        ];
     }
 }
